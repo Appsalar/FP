116f6cf75d380499b35aa0741304394ca3e72c73
Ivan Velkov <sasmose@yahoo.com>
2017-02-01 17:24:57 +0200

    Template working

--- Huffman/huffman.hs
+++ Huffman/huffman.hs
@@ -5,12 +5,19 @@ import qualified Data.List as List
 foo::Map.Map Char Int
 foo = Map.fromList []
 
-freqCnt [] cont = cont
-freqCnt (x:xs) cont = freqCnt xs $ Map.insertWith (+) x 1 cont
+insertFreqLst:: (a -> a -> Bool)-> a -> [(a, Int)] -> [(a, Int)]
+insertFreqLst _ el [] = [(el,1)]
+insertFreqLst f el (x:xs)
+	| f el $ fst x = (fst x, snd x + 1):xs
+	| otherwise = x:insertFreqLst f el xs
 
-data Tree = Leaf Char Int | Node Int Tree Tree deriving Show
+freqCnt::(a -> a -> Bool)-> [a] -> [(a, Int)] -> [(a, Int)]
+freqCnt _ [] cont = cont
+freqCnt f (x:xs) cont = freqCnt f xs $ insertFreqLst f x cont
 
-t::Tree
+data Tree a = ET | Leaf a Int | Node Int (Tree a) (Tree a) deriving Show
+
+t::Tree Char
 t = (Node 12
 	(Leaf 'a' 5)
 	(Node 123
@@ -19,42 +26,44 @@ t = (Node 12
 			(Leaf 'c' 12)
 			(Leaf 'd' 123))))
 
-instance Eq Tree where
-	(Leaf _ x) == (Leaf _ y) = x == y
-	(Node x _ _) == (Leaf _ y) = x == y
-	(Leaf _ x) == (Node y _ _) = x == y
-	(Node x _ _) == (Node y _ _) = x == y
-	
-instance Ord Tree where
-	compare (Leaf _ x) (Leaf _ y) = compare x y
-	compare (Node x _ _) (Leaf _ y) = compare x y
-	compare (Leaf _ x) (Node y _ _) = compare x y
-	compare (Node x _ _) (Node y _ _) = compare x y
+getFreq::Tree a -> Int
+getFreq ET = 0
+getFreq (Leaf _ x) = x
+getFreq (Node x _ _) = x
 
-genLeafs cont = map (\ (key, freq)-> (Leaf key freq)) $ Map.toList cont
+instance Eq (Tree x) where
+	a == b = getFreq a == getFreq b
 	
+instance Ord (Tree x) where
+	compare a b = compare (getFreq a) (getFreq b)
+
+genLeafs cont = map (\ (key, freq)-> (Leaf key freq)) cont
+
+createHuffmanTree:: [Tree a] -> Tree a
+createHuffmanTree [] = ET
 createHuffmanTree lst = head $ helper $ List.sort lst where
-	createNode l@(Leaf _ x)   r@(Leaf _ y) = (Node (x + y) r l)
-	createNode l@(Node x _ _) r@(Leaf _ y) = (Node (x + y) r l)
-	createNode l@(Leaf _ x)   r@(Node y _ _) = (Node (x + y) r l)
-	createNode l@(Node x _ _) r@(Node y _ _) = (Node (x + y) r l)
+	createNode l r = (Node (getFreq l + getFreq r) r l)
 	helper (x:xs)
 		| null xs = [x]
 		| otherwise = helper $ List.sort ((createNode x $ head xs) : tail xs)
 
-genHuffmanCode:: Tree -> [(Char, String)]
-genHuffmanCode (Node _ lt rt) = map (\ (x:xs)-> (x, xs))
-	$ (genSingleCode lt "0") ++ (genSingleCode rt "1") where
+genHuffmanCode:: Tree a -> [(a, String)]
+genHuffmanCode (Node _ lt rt) = 
+	(genSingleCode lt "0") ++ (genSingleCode rt "1") where
 	genSingleCode (Node _ lt rt) code = 
 		(genSingleCode lt (code ++ "0") ) ++ (genSingleCode rt (code ++ "1"))
-	genSingleCode (Leaf x _) code = [x:code]
+	genSingleCode (Leaf x _) code = [(x, code)]
 
+genCompressed::  (a -> a -> Bool)-> [a] -> [(a, String)] -> String
+genCompressed _ [] _ = []
+genCompressed f (x:xs) cont = lookupDummyMap f x cont ++ genCompressed f xs cont where
+	lookupDummyMap _ _ [] = []
+	lookupDummyMap f el (x:xs)
+		| f el $ fst x = snd x
+		| otherwise = lookupDummyMap f el xs
 
-genCompressed [] _ = []
-genCompressed (x:xs) cont = (Mb.fromJust (Map.lookup x cont)) ++ genCompressed xs cont
-
-compress str = genCompressed str $ Map.fromList
-	$ genHuffmanCode $ createHuffmanTree $ genLeafs $ freqCnt str foo
+compress f str = genCompressed f str
+	$ genHuffmanCode $ createHuffmanTree $ genLeafs $ freqCnt f str []
 
 
 genFromHuffman str tree = genFromCompress str tree where
