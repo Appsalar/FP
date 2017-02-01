fdd66df65c6e7b3e9cd8cff8a944e68c311eac3b
Ivan Velkov <sasmose@yahoo.com>
2017-02-01 22:46:10 +0200

    added ugly decoding

--- Huffman/main.hs
+++ Huffman/main.hs
@@ -2,7 +2,7 @@ module Main where
 import Huffman
 import System.Environment (getArgs)
 import System.IO (writeFile)
-	
+
 main = do 
 	args <- getArgs
 	if length args < 3 then
@@ -10,9 +10,23 @@ main = do
 	else do
 		let (command:fileName:outFilename:_) = args
 		putStrLn $ "Operation: " ++ command
-		putStrLn $ "Source: " ++ fileName
-		str <- readFile fileName
-		putStrLn $ "Destination: " ++ outFilename
-		writeFile outFilename $ (show $ snd $ compress (==) str) 
-			++ "\n" ++ (fst $ compress (==) str)
+		if command == "encode" then do
+			putStrLn $ "Source: " ++ fileName
+			str <- readFile fileName
+			let a = (read str :: [Int])
+			print a
+			putStrLn $ "Destination: " ++ outFilename
+			writeFile outFilename $ (show $ snd $ compress (==) a) 
+				++ "\n" ++ (fst $ compress (==) a)
+		else do
+			putStrLn $ "Source: " ++ fileName
+			str <- readFile fileName
+			let a = lines str
+			--print a
+			putStrLn $ "Destination: " ++ outFilename
+			let t = (read (head a) :: Tree Int)
+			print $ show t
+			let cod = a !! 1
+			print cod
+			writeFile outFilename $ show (genFromHuffman cod t)
 		putStrLn $ "done."
