426faf32c68cad4667c3b49d72e97a5cb7379314
Ivan Velkov <sasmose@yahoo.com>
2017-02-13 16:19:06 +0200

    sth

--- Animals/main.hs
+++ Animals/main.hs
@@ -1,6 +1,7 @@
 module Main where
 import Zoo
 import System.IO
+import System.Directory
 
 animals:: Tree
 animals = Question "Лети ли?"
@@ -12,10 +13,14 @@ fileName = "data.txt"
 
 
 main = do 
-		hSetBuffering stdin NoBuffering
-		h <- openFile fileName ReadMode
-		db <- hGetContents h
-		let t = (read db :: Tree)
-		newDb <- play t
-		hClose h
-		writeFile fileName $ show newDb
+		fileExists <- doesFileExist fileName
+		if fileExists then do
+			h <- openFile fileName ReadMode
+			db <- hGetContents h
+			let t = (read db :: Tree)
+			newDb <- play t
+			hClose h
+			writeFile fileName $ show newDb
+		else do
+			newDb <- play animals
+			writeFile fileName $ show newDb
